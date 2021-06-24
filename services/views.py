fb86cabb265cbec365d25eb4299221f1ce775eff
dudeguythethird <keablejoseph@gmail.com>
2021-06-24 13:12:51 +0000

    completes text content of services page

--- services/views.py
+++ services/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def services(request):
+    """A view to show the services explanation page."""
+
+    return render(request, 'services/services.html')
+
+
+def products(request):
+    """A view to show all stocked products, as well as sorting them."""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'services/products.html', context) 
