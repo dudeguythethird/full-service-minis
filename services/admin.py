56da46988ff5903a4e9e91acc451d88da4e0621b
dudeguythethird <keablejoseph@gmail.com>
2021-06-24 09:27:51 +0000

    adds products, categories and paint tiers to database, and customises admin for them

--- services/admin.py
+++ services/admin.py
@@ -2,6 +2,28 @@ from django.contrib import admin
 from .models import Product, Category, PaintTier
 
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(PaintTier)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'base_price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class PaintTierAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price_multiplier'
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(PaintTier, PaintTierAdmin)
--- services/models.py
+++ services/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -28,7 +32,7 @@ class Product(models.Model):
 class PaintTier(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField(null=True, blank=True)
-    price_multiplier = models.DecimalField(max_digits=6, decimal_places=2)
+    price_multiplier = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return self.name
